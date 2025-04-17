e9fb1b2200920e298e0e7ce2466c5fbf96b7e431
Santiagog <santiagog5296@gmail.com>
2025-04-17 17:00:11 -0500

    migraciones buenas

--- laravel/database/migrations/2025_04_17_210621_create_carts_table.php
+++ laravel/database/migrations/2025_04_17_210621_create_carts_table.php
@@ -13,18 +13,14 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
+            $table->foreignId('product_id')->constrained('products');
             $table->integer('cantidad');
-            $table->unsignedBigInteger('pago_id')->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('status_id');
+            $table->foreignId('card_id')->nullable()->constrained('cards')->nullOnDelete();
+            $table->foreignId('payment_method_id')->nullable()->constrained('payment_method')->nullOnDelete();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('status_id')->constrained('statuses');
             $table->decimal('total', 10, 2);
             $table->timestamps();
-    
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('pago_id')->references('id')->on('pagos')->nullOnDelete();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('status_id')->references('id')->on('statuses');
         });
     }
 
